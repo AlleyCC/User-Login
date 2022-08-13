fbf52a5766f141ab787a3012d29d9b8dd0337e53
AlleyChang <yifanchang0602@gmail.com>
2022-08-13 16:58:39 +0800

    feat: add cookie authentication

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require('express')
-const app = express()
 const { engine } = require('express-handlebars')
-const port = 3000
 const bodyParser = require('body-parser')
 const Users = require('./models/users')
+const session = require('express-session')
+
+const app = express()
+const port = 3000
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true}))
-
+app.use(session({
+  secret: 'mySecret',
+  name: 'user',
+  resave: true,
+  saveUninitialized: false,
+}))
 
 app.get('/', (req, res) => {
+  console.log('req.session', req.session)
+  console.log('req.session.user', req.session.user)
+  console.log('req.sessionID', req.sessionID)
   res.render('index')
 })
+
+function auth(req, res, next){
+  if(req.session.user){
+    console.log('authenticated')
+    next()
+  } else {
+    console.log('not authenticated!')
+    return res.redirect('/')
+  }
+  
+}
 
 app.post('/login', (req, res) => {
   const email = req.body.email
@@ -20,11 +41,19 @@ app.post('/login', (req, res) => {
   const filteredUser = Users.find(item => item.email === email && item.password === password)
   
   if (filteredUser){
+    req.session.user = filteredUser.firstName
     return res.render('login', { filteredUser })
   } else {
-    const alert = true
-    return res.render('index', { alert })
+    
+    return res.render('index', { alert: 'Username/Password incorrect! Please enter again.' })
   } 
+})
+
+app.get('/logout', auth, (req, res) => {
+  req.session.destroy(() => {
+    console.log('session destroyed')
+  })
+  res.render('index', { alert: 'You are logged out! Re-enter email and password to log in again!' })
 })
 
 app.listen(port, (req, res) => {
